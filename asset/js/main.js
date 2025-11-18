3d09dcd3109393a610b2004f9bd454c182cf6964
giftin.id <calistanafisaa@gmail.com>
2025-11-18 17:22:19 +0700

    Update greeting text from 'Hai, Lovee' to 'Hi, Krissy'

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Hai, Lovee'.split('')
+const text = 'Hi, Krissy'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
